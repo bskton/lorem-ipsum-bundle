58f58a40909ef4fbe955abbc0e7c4391c88d5d88
Ilya Levin <bskton@gmail.com>
2020-04-11 14:03:26 +1000

    Filter API response event

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -4,7 +4,80 @@
 namespace KnpU\LoremIpsumBundle\Tests;
 
 
-class FunctionalTest
+use KnpU\LoremIpsumBundle\KnpUIpsum;
+use KnpU\LoremIpsumBundle\KnpULoremIpsumBundle;
+use KnpU\LoremIpsumBundle\WordProviderInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use function foo\func;
+
+class FunctionalTest extends TestCase
 {
+    public function testServiceWiring()
+    {
+        $kernel = new KnpULoremIpsumTestingKernel();
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        /** @var KnpUIpsum $ipsum */
+        $ipsum = $container->get('knpu_lorem_ipsum.knpu_ipsum');
+        $this->assertInstanceOf(KnpUIpsum::class, $ipsum);
+        $this->assertIsString($ipsum->getParagraphs());
+    }
+
+    public function testServiceWiringConfiguration()
+    {
+        $kernel = new KnpULoremIpsumTestingKernel([
+            'word_provider' => 'stub_word_list'
+        ]);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        /** @var KnpUIpsum $ipsum */
+        $ipsum = $container->get('knpu_lorem_ipsum.knpu_ipsum');
+        $this->assertStringContainsString('stub', $ipsum->getWords(2));
+    }
+}
+
+class KnpULoremIpsumTestingKernel extends Kernel
+{
+    private array $knpUIpsumConfig;
+
+    public function __construct(array $knpUIpsumConfig = [])
+    {
+        $this->knpUIpsumConfig = $knpUIpsumConfig;
+
+        parent::__construct('test', true);
+    }
+
+    public function registerBundles()
+    {
+        return [
+            new KnpULoremIpsumBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function(ContainerBuilder $container) {
+            $container->register('stub_word_list', StubWordList::class);
+            $container->loadFromExtension('knpu_lorem_ipsum', $this->knpUIpsumConfig);
+        });
+    }
+
+    public function getCacheDir()
+    {
+        return __DIR__.'/../var/cache/test/'.spl_object_hash($this);
+    }
+}
+
+class StubWordList implements WordProviderInterface
+{
+    public function getWordList(): array
+    {
+        return ['stub', 'stub2'];
+    }
 
 }
